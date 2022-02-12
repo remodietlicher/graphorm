fb8fea4c4dd02dc0a75d4f488e57c9e85d218457
Remo Dietlicher <remo.dietlicher@meteoswiss.ch>
2022-02-12 22:58:15 +0100

    simplify

--- src/entity/SubjectManager.ts
+++ src/entity/SubjectManager.ts
@@ -1,10 +1,7 @@
 import { Connection } from "../connection/Connection";
-import { ComunicaQueryRunner } from "../driver/comunica/ComunicaQueryRunner";
 
 export class SubjectManager {
   readonly connection: Connection;
-
-  readonly queryRunner?: ComunicaQueryRunner;
 
   constructor(connection: Connection) {
     this.connection = connection;
