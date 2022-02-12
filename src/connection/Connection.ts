c471aab5466a76a7be9abba1a832464d2b4bc9e2
Remo Dietlicher <remo.dietlicher@meteoswiss.ch>
2022-02-12 22:19:43 +0100

    add example for full 1-1 query

--- src/connection/Connection.ts
+++ src/connection/Connection.ts
@@ -1,7 +1,58 @@
 import { EntityManager } from "../entity/EntityManager";
 
-export class Connection {
-  manager: EntityManager;
+/*
+Target:
+-------
+SELECT * WHERE {
+  ?x a Thesis.
+  ?x isPartOf ?y.
+  ?z hasObtained ?y.
+  ?z name "Remo".
+  ?x topic ?res.
+}
 
-  constructor() {}
+Person:
+-------
+SELECT ?name ?diploma ?email ?adress WHERE {
+  ?x a Person;
+     name ?name;
+     hasObtained ?diploma;
+     hasEmail ?email;
+     address ?adress.
+  ?diploma a Diploma.
+}
+
+Thesis:
+-------
+SELECT ?topic ?supervisor ?associatedDiploma WHERE {
+  ?x a Thesis;
+     hasTopic ?topic;
+     supervisedBy ?supervisor;
+     partOf ?associatedDiploma.
+}
+
+Combined Thesis + Person:
+-------------------------
+SELECT ?topic WHERE {
+  ?x1 a Thesis;
+      hasTopic ?topic;
+      supervisedBy ?supervisor;
+      partOf ?associatedDiploma.  <----- ?diploma
+
+  ?x2 a Person;
+      name ?name;                 <----- "Remo"
+      hasObtained ?diploma;
+      hasEmail ?email;
+      address ?adress.
+  ?diploma a Diploma.
+}
+
+*/
+
+export class Connection {
+  readonly manager: EntityManager;
+
+  constructor(manager: EntityManager) {
+    this.manager = manager;
+  }
 }
